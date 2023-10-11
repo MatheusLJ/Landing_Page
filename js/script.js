2e7100c595e936b7c70dc1e65276795cb2d1e3ad
MatheusLJ <mleitejesus@gmail.com>
2023-10-11 17:54:55 -0300

    att

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const control1 = document.querySelector('#quadrado1');
+const control2 = document.querySelector('#quadrado2');
+const control3 = document.querySelector('#quadrado3');
+const control4 = document.querySelector('#quadrado4');
+const items = document.querySelectorAll('.section');
+let currentItem = 0;
+
+setInterval(function(){
+    if (currentItem< items.length) {
+        
+    }
+}, 1000)
